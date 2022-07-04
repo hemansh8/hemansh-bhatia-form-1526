7b6426b2ed9349af5d8cdf0a34236a7ef5056739
hemansh8 <52319420+hemansh8@users.noreply.github.com>
2022-07-04 09:58:14 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+document.querySelector('form button').addEventListener('click', function (e) {
+    let outputObj = {};
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const message = document.querySelector('#message');
+    if (!name.value || !email.value || !message.value) {
+        e.preventDefault();
+        if (!name.value) {
+            outputObj['fn'] = "Full name is missing";
+        }
+        if (!email.value) {
+            outputObj['em'] = "Email is missing";
+        }
+        if (!message.value) {
+            outputObj['msg'] = "Message is missing";
+        }
+        console.log('Errors ');
+        console.log(outputObj);
+    }
+});
+
+document.querySelector('form').addEventListener('submit', function () {
+    let outputObj = {};
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+    const message = document.querySelector('#message');
+    outputObj['fullname'] = name.value;
+    outputObj['email'] = email.value;
+    outputObj['message'] = message.value;
+    console.log('Collected Data');
+    console.log(outputObj);
+    name.value = "";
+    email.value = "";
+    message.value = "";
+});
